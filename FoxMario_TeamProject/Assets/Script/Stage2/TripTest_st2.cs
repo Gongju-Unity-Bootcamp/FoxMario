86277a496ddedcd598d836211d39bba6aac8a728
2gangmin <gm010815@gmail.com>
2023-11-19 20:46:29 +0900

    Create Stage2 Triggers

--- FoxMario_TeamProject/Assets/Script/Stage2/TripTest_st2.cs
+++ FoxMario_TeamProject/Assets/Script/Stage2/TripTest_st2.cs
@@ -4,56 +4,91 @@ using UnityEngine;
 
 public class TripTest_st2 : MonoBehaviour
 {
-    public Collider2D triggerA;
-    public Collider2D triggerB;
-    public GameObject wallA;
-    public GameObject fishC;
-    private float timerA = 10f;
-    private bool testA = false;     // 디버그 확인
+    public Collider2D TriggerA;
+    public Collider2D TriggerB;
+    public Collider2D TriggerC;
+    public Collider2D TriggerD;
+    public Collider2D TriggerE;
+    public Collider2D TriggerF;
+    public Collider2D TriggerG;
+
+
+    public GameObject WallA;
+    public GameObject FishC;
+    public GameObject TrapC;
+
+    public Rigidbody2D TrapD;
+
+    private float TimerA = 10f;
+    private bool DeleteWallA = false;
+    private float DeleteTime = 5f;
 
     private void Update()
     {
-        timerA -= Time.deltaTime ;
+        BreakWallA();
 
-        if (testA && timerA < 0f)
-        {
-            if (wallA != null)
-            {
-                wallA.SetActive(false);
-            }
-            
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            if (gameObject.CompareTag("Trigger2-A"))
+            if (gameObject.CompareTag("Trig2-A"))
             {
-                //Debug.Log("Player touched Trigger A");
-                //testA = true;
+                DeleteWallA = true;
+                TriggerA.enabled = false;
 
                 // 오브젝트 B를 찾아서 비활성화
-                if (triggerB != null)
+                if (TriggerB != null)
                 {
-                    triggerB.enabled = false;
+                    TriggerB.enabled = false;
                 }
-                triggerA.enabled = false;
             }
-            // 플레이어가 오브젝트 B에 닿았을 때
-            else if (gameObject.CompareTag("Trigger2-B"))
+            else if (gameObject.CompareTag("Trig2-B"))
             {
-                //Debug.Log("Player touched Trigger B");
-                triggerA.enabled = false;
+                TriggerA.enabled = false;
+                TriggerB.enabled = false;
             }
+            else if (gameObject.CompareTag("Trig2-C"))
+            {
+                TrapC.SetActive(true);
+            }
+            else if (gameObject.CompareTag("Trig2-D"))
+            {
+                TrapD.simulated = true;
+            }
+            else if (gameObject.CompareTag("Trig2-E"))
+            {
 
-            else if (gameObject.CompareTag("Trigger2-C"))
+            }
+            else if (gameObject.CompareTag("Trig2-F"))
             {
-                // fishC.transform.position.y = new Vector3();
+                
+                if (TriggerG != null)
+                {
+                    TriggerG.enabled = false;
+                }
+                TriggerF.enabled = false;
+            }
+            else if (gameObject.CompareTag("Trig2-G"))
+            {
+                TriggerG.enabled = false;
             }
         }
+    }
 
+    private void BreakWallA()
+    {
+        TimerA -= Time.deltaTime;
+
+        if (DeleteWallA && TimerA < 0f)
+        {
+            if (WallA != null)
+            {
+                WallA.SetActive(false);
+            }
+
+        }
     }
 }
 
