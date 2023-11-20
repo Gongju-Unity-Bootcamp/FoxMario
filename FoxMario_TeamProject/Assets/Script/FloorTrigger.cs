db33f898ddc446d2329089e6860ac3170c38d7d7
Jinho Lee <bravearc69@gmail.com>
2023-11-20 13:40:45 +0900

    st1_모듈화 시작

--- FoxMario_TeamProject/Assets/Script/FloorTrigger.cs
+++ FoxMario_TeamProject/Assets/Script/FloorTrigger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -7,16 +8,16 @@ public class NewBehaviourScript : MonoBehaviour
 
     //public float speed = 50f;
     private Rigidbody rigid;
+    //private GameObject[] traps;
 
     void Start()
     {
 
     }
 
-    private void OnTriggerEnter2D(Collider2D ohder)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-
-        if (ohder.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             rigid = GetComponent<Rigidbody>();
             //rigid.velocity = new Vector3(0, speed, 0);
---
+++ FoxMario_TeamProject/Assets/Script/TrapManager.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class FallTrap
+{
+    public string name;
+    public GameObject gameObject;
+    public bool isActived;
+    public float scale;
+}
+
+public class TrapManager : MonoBehaviour
+{
+    [SerializeField] private FallTrap[] fallTrap;
+
+    private void OnTriggerEnter2D(Collider2D _hit)
+    {
+        if (_hit.gameObject.CompareTag("Player"))
+        {
+            for (int i = 0; i < fallTrap.Length; ++i)
+            {
+                if (_hit.gameObject == fallTrap[i].gameObject)
+                {
+                    fallTrap[i].isActived = true;
+                    fallTrap[i].gameObject.GetComponent<Rigidbody2D>().simulated = false;
+                    fallTrap[i].gameObject.GetComponent<Rigidbody2D>().gravityScale = fallTrap[i].scale;
+                }
+            }
+        }
+    }
+}
