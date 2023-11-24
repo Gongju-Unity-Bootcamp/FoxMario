c1c8f604dd2c0f62f6c44ffaa9501738e13acb8f
KimCastle07 <rpaehfdl741@gmail.com>
2023-11-24 14:10:03 +0900

    Charactor and Stage1 Final Complete

--- FoxMario_TeamProject/Assets/Script/SceneBack.cs
+++ FoxMario_TeamProject/Assets/Script/SceneBack.cs
@@ -1,34 +1,34 @@
-using Ending;
-using Player;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
-using Unity.VisualScripting;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+//using Ending;
+//using Player;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Threading;
+//using Unity.VisualScripting;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-namespace Back
-{
-    public class SceneBack : MonoBehaviour
-    {
-        public void Update()
-        {
-            // Space 클릭시 GoPreviousScene 실행
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                PlayerDie.isDied = false;
-                GoPreviousScene();
-            }
-            // R키 입력시 사망카운트 초기화
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                PlayerPrefs.DeleteAll();
-            }
-        }
+//namespace Back
+//{
+//    public class SceneBack : MonoBehaviour
+//    {
+//        public void Update()
+//        {
+//            // Space 클릭시 GoPreviousScene 실행
+//            if (Input.GetKeyDown(KeyCode.Space))
+//            {
+//                PlayerDie.isDied = false;
+//                GoPreviousScene();
+//            }
+//            // R키 입력시 사망카운트 초기화
+//            if (Input.GetKeyDown(KeyCode.R))
+//            {
+//                PlayerPrefs.DeleteAll();
+//            }
+//        }
 
-        public void GoPreviousScene()
-        {   // 인게임 씬으로 전환
-            SceneManager.LoadScene("EndingText2");
-        }
-    }
-}
+//        public void GoPreviousScene()
+//        {   // 인게임 씬으로 전환
+//            SceneManager.LoadScene("Stage1");
+//        }
+//    }
+//}
